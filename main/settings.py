bb11de0df220763148d3907c82468f99c78e7bac
Albert Caballero <albertcaballerocoll@gmail.com>
2025-01-10 00:58:09 +0100

    whyyyy

--- main/settings.py
+++ main/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'website', 'static_production')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website', 'static'),]
 
-STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.FileSystemFinder']
+STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.AppDirectoriesFinder']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
