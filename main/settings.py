084ef1b4997439bcaf357adf6b2d3f660e626bc5
Albert Caballero <albertcaballerocoll@gmail.com>
2025-01-16 19:28:09 +0100

    hand recorder update

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#^j%sd3-!j%nuyp7vdq&c3ar1s*l*1!8wh5-11f9al=i4hl@p('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'albertcab.com', '127.0.0.1']
 
@@ -123,7 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'website', 'static_production')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website', 'static'),]
 
-STATICFILES_FINDERS = ['django.contrib.staticfiles.finders.AppDirectoriesFinder']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
